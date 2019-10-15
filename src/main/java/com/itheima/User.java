b8397d2d25c12380bc48d9c051723ad568a76138
zzz <1074893272@qq.com>
2019-10-15 09:38:55 +0800

    修改User

--- src/main/java/com/itheima/User.java
+++ src/main/java/com/itheima/User.java
@@ -1,4 +1,17 @@
 package com.itheima;
 
 public class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
